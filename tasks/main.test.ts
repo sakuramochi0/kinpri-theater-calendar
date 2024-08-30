5559d3349d4641a81c9887a101c4c01a2d10f440
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-08-30 10:40:29 +0900

    refactor: replace theaterName with screenName

--- tasks/main.test.ts
+++ tasks/main.test.ts
@@ -7,9 +7,10 @@ test('新宿バルト9', async ({ page }) => {
   const date = await page.locator('.calendar-item.calendar-active').evaluate(e=>e.dataset.date)
 
   const schedules = await scheduleBox.evaluateAll(schedules => schedules.map(schedule => {
-    const theaterName = schedule.querySelector('.theater-name').textContent.trim()
+    const screenName = schedule.querySelector('.theater-name').textContent.trim()
     const timeString = schedule.querySelector('.schedule-time').textContent.trim()
     const [[_, startTime, endTime]] = timeString.matchAll(/(\d+:\d+)\D+(\d+:\d+)/g)
+    return {screenName, startTime, endTime}
     return {theaterName, startTime, endTime}
   }))
   const data = schedules.map(schedule=>{
