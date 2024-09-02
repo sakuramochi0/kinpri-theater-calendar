4be7c646fde4d6e2bf36deec0caf11aac261009c
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-09-02 18:11:03 +0900

    feat: generate `README.md` dynamically from existing ical and json files

---
+++ generate-readme.ts
@@ -0,0 +1,30 @@
+import { globSync, readFileSync, writeFileSync } from 'node:fs'
+
+import Mustache from 'mustache'
+
+type Calendar = {
+  theaterName: string
+  icalURL: string
+  jsonURL: string
+}
+
+function main() {
+  const template = readFileSync('README.md.mustache', 'utf-8')
+  const calendars: Calendar[] = getCalendars()
+  const rendered = Mustache.render(template, { calendars })
+  writeFileSync('README.md', rendered)
+}
+
+function getCalendars(): Calendar[] {
+  const icalFilenames = globSync('data/*.ics')
+  return icalFilenames.map(ical => {
+    const rawBasename = ical.replace('.ics', '').replace('data/', '')
+    const theaterName = rawBasename.replace(' 『KING OF PRISM -Dramatic PRISM.1-』上映時間', '')
+    const basename = encodeURIComponent(rawBasename)
+    const icalURL = `https://kinpri-theater-calendar.skrm.ch/data/${basename}.ics`
+    const jsonURL = `https://kinpri-theater-calendar.skrm.ch/data/${basename}.json`
+    return {theaterName, icalURL, jsonURL}
+  })
+}
+
+main()
