f335e2a27ca1c4eed6a85cb5148fbe751d041087
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-09-04 13:09:11 +0900

    feat(tjoy): include title and status

--- tasks/tjoy.ts
+++ tasks/tjoy.ts
@@ -36,36 +36,63 @@ export async function getTjoySchedules(page: Page, url: string, theaterName: str
 }
 
 async function getTjoyDailySchedules(page: Page) {
-  const scheduleBoxes = await page.locator('.schedule-box')
   const date = await page.locator('.calendar-item.calendar-active').evaluate(e => e.dataset.date)
 
-  const rawSchedules = await scheduleBoxes.evaluateAll(
-    scheduleBoxes => scheduleBoxes.map(
-      scheduleBox => {
-        const screenName = scheduleBox.querySelector('.theater-name')!.textContent!.trim()
-        const timeString = scheduleBox.querySelector('.schedule-time')!.textContent!.trim()
-        const [[_, startTime, endTime]] = timeString.matchAll(/(\d+:\d+)\D+(\d+:\d+)/g)
-        return { screenName, startTime, endTime }
-      }
+  const movies = page.locator('.section-container')
+  const schedules: Schedule[] = (await movies.evaluateAll(
+      (movies, date) => movies.map(
+        movie => {
+          const title = movie.querySelector('.js-title-film')!.textContent!.trim()
+          const shows = [...movie.querySelectorAll('.schedule-box')]
+          return shows.map(
+            show => {
+              const screenName = show.querySelector('.theater-name')!.textContent!.trim()
+              const timeString = show.querySelector('.schedule-time')!.textContent!.trim()
+              let [[_, startTimeString, endTimeString]] = timeString.matchAll(/(\d+:\d+)\D+(\d+:\d+)/g)
+
+              // FIXME: handle after 24:00. use dayjs etc.
+              const startHour = parseInt(startTimeString.split(':')[0])
+              const endHour = parseInt(endTimeString.split(':')[0])
+
+              if (startHour >= 24) {
+                startTimeString = '23:59'
+              }
+              if (endHour >= 24) {
+                endTimeString = '23:59'
+              }
+              const startTime = new Date(`${date} ${startTimeString} GMT+0900`)
+              const endTime = new Date(`${date} ${endTimeString}  GMT+0900`)
+
+              const statusText = show.querySelector('.schedule-status')!.textContent!.trim()
+
+              let statusSymbol: string
+              const icon = show.querySelector('.schedule-status img.icon-top')!
+              const src = icon.getAttribute('src')!
+              if (src.includes('icon_label3')) {
+                statusSymbol = '◎'
+              } else if (src.includes('icon_label2')) {
+                statusSymbol = '○'
+              } else if (src.includes('icon_label4')) {
+                statusSymbol = '△'
+              } else if (src.includes('icon_label5')) {
+                statusSymbol = '☓'
+              } else if (src.includes('icon_label1')) {
+                statusSymbol = '-'
+              } else {
+                statusSymbol = ''
+              }
+
+              const status = `${statusSymbol}${statusText}`
+              return { title, screenName, startTime, endTime, status }
+            }
+          )
+        }
+      ),
+      date
     )
   )
-
-  const schedules: Schedule[] = rawSchedules.map(schedule => {
+    .flat(2)
     // FIXME: handle after 24:00. use dayjs etc.
-    const startHour = parseInt(schedule.startTime.split(':')[0])
-    const endHour = parseInt(schedule.endTime.split(':')[0])
-    if (startHour >= 24) {
-      schedule.startTime = '23:59'
-    }
-    if (endHour >= 24) {
-      schedule.endTime = '23:59'
-    }
-
-    const startTime = new Date(`${date} ${schedule.startTime} GMT+0900`)
-    const endTime = new Date(`${date} ${schedule.endTime}  GMT+0900`)
-
-    return { ...schedule, startTime, endTime }
-  })
     .filter(e => isValidDate(e.startTime))
 
   return schedules
