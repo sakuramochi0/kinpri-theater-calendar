749ff9e7b9652ffe864b0aa27d7702d19228d01d
sakuramochi <sakuramochi.mochi.0@gmail.com>
2024-09-07 01:30:59 +0900

    feat: support aeon cinema series (only one)

---
+++ tasks/aeonCinema.test.ts
@@ -0,0 +1,52 @@
+import { expect, test } from '@playwright/test';
+
+import { generateICal, saveJSON } from './utils';
+
+import { Schedule, Theater } from './types';
+
+test('イオンシネマ系列', async ({ page }) => {
+  await page.goto('https://www.aeoncinema.com/')
+  await page.getByRole('link', { name: /作品案内/ }).first().click()
+
+  await page.locator('.cinemaBlock')
+    .filter({ hasText: /KING OF PRISM/ })
+    .locator('.cbTitle')
+    .click()
+
+  const theaterList = page.locator('.table_sakuhinpage_screeningStatus').first()
+  await expect(theaterList).toBeVisible()
+
+  const theaters: Theater[] = await theaterList.locator('a').evaluateAll(links => links.map(theaterLink => ({
+    name: theaterLink.textContent,
+    url: theaterLink.href,
+  })))
+  for (const theater of theaters.slice(0, 1)) { // TODO: support other theaters
+    await page.goto(theater.url)
+
+    const schedules: Schedule[] = []
+
+    const year = new Date().getFullYear()
+    const [monthString, dayString] = (await page.locator('.today').first().textContent())!.match(/\d+/g)
+    const dateString = `${year}/${monthString}/${dayString}`
+
+    await expect(page.locator('.movielist').last()).toBeVisible()
+    const movies = await page.locator('.movielist').filter({ hasText: /KING OF PRISM/ }).all()
+    for (const movie of movies) {
+      const title = (await movie.locator('.main a').first().textContent() ?? '').trim()
+      const shows = (await movie.locator('.timetbl > div').all()).slice(1, -1)
+      for (const show of shows) {
+        const timeString = await show.locator('.time').first().textContent()
+        const [startHour, startMinute, endHour, endMinute] = timeString.match(/\d+/g)
+        const startTime = new Date(`${dateString} ${startHour}:${startMinute}`)
+        const endTime = new Date(`${dateString} ${endHour}:${endMinute}`)
+        const screenName = (await show.locator('.screen').first().textContent())?.trim() ?? ''
+        const status = (await show.locator('.txt_btn').first().textContent())?.trim() ?? ''
+        schedules.push({ title, startTime, endTime, screenName, status })
+      }
+    }
+
+    console.log(schedules)
+    saveJSON(theater.name, schedules)
+    generateICal(theater.name, theater.url, schedules)
+  }
+})
